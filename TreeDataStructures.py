55cde91897696bd181ccaa047759e5a83922c9d2
Ben Spencer <61650582+Ben-Spencer@users.noreply.github.com>
2020-07-16 18:14:02 -0400

    Create TreeDataStructures.py

---
+++ TreeDataStructures.py
@@ -0,0 +1,14 @@
+<h1>Tree Data Structure</h1>
+Trees are non-linear, hierarchical data structures. This abstract data type (ADT) is composed of the following:</br>
+<ul>
+  <li>Node: </li>
+  <li>Edge: </li>
+  <li>Root: </li>
+  <li>Parent: </li>
+  <li>Child: </li>
+  <li>Siblings: </li>
+  <li>Leaves: </li>
+</ul>
+The depth of a node is the number of edges from the root to the node.<br>
+The height of the tree is the longest possible path from the root to the leaves.<br>
+<h2>General Tree</h2>
